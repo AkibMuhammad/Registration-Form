942b4b122aa731743b6550d82fa54f0007a8caef
Akib Muhammad <87432746+AkibMuhammad@users.noreply.github.com>
2021-10-28 05:03:49 +0600

    Add files via upload

---
+++ First.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset="utf-8">
+	 <title>Form</title>
+</head>
+<body>
+    <h1>Form</h1>
+
+    <form action="FormAction.php" method="POST">First Name: <input type="text" name="firstname"><br><br>Last Name: <input type="text" name="lastname"><br><br>
+    <input type="submit" name="submit">
+ </form>
+ 
+ </body>
+ </html>   	 
